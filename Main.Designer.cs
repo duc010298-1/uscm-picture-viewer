7699e54fecc9b906bd9b935046d05a5ab8d86119
Đỗ Trung Đức <duc010298@gmail.com>
2019-10-12 20:55:43 +0700

    best default size

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -36,7 +36,6 @@
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitItemMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
-            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.buttonSelectArea = new System.Windows.Forms.Button();
             this.buttonCropManual = new System.Windows.Forms.Button();
             this.pictureBox = new System.Windows.Forms.PictureBox();
@@ -44,6 +43,7 @@
             this.buttonCropNormal = new System.Windows.Forms.Button();
             this.buttonCropFace = new System.Windows.Forms.Button();
             this.buttonUndo = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.menu.SuspendLayout();
             this.tableLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
@@ -55,7 +55,7 @@
             this.openToolStripMenuItem});
             this.menu.Location = new System.Drawing.Point(0, 0);
             this.menu.Name = "menu";
-            this.menu.Size = new System.Drawing.Size(1146, 24);
+            this.menu.Size = new System.Drawing.Size(1084, 24);
             this.menu.TabIndex = 2;
             this.menu.Text = "menu";
             // 
@@ -118,12 +118,8 @@
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayout.Size = new System.Drawing.Size(1146, 713);
+            this.tableLayout.Size = new System.Drawing.Size(1084, 713);
             this.tableLayout.TabIndex = 0;
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.FileName = "openFileDialog1";
             // 
             // buttonSelectArea
             // 
@@ -133,7 +129,7 @@
             this.buttonSelectArea.Image = global::PictureViewer.Properties.Resources.select;
             this.buttonSelectArea.Location = new System.Drawing.Point(3, 666);
             this.buttonSelectArea.Name = "buttonSelectArea";
-            this.buttonSelectArea.Size = new System.Drawing.Size(566, 44);
+            this.buttonSelectArea.Size = new System.Drawing.Size(536, 44);
             this.buttonSelectArea.TabIndex = 6;
             this.buttonSelectArea.Text = "Chọn vùng";
             this.buttonSelectArea.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -147,9 +143,9 @@
             this.buttonCropManual.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonCropManual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonCropManual.Image = global::PictureViewer.Properties.Resources.crop_VBF_icon;
-            this.buttonCropManual.Location = new System.Drawing.Point(575, 666);
+            this.buttonCropManual.Location = new System.Drawing.Point(545, 666);
             this.buttonCropManual.Name = "buttonCropManual";
-            this.buttonCropManual.Size = new System.Drawing.Size(568, 44);
+            this.buttonCropManual.Size = new System.Drawing.Size(536, 44);
             this.buttonCropManual.TabIndex = 5;
             this.buttonCropManual.Text = "Cắt thủ công";
             this.buttonCropManual.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -165,7 +161,7 @@
             this.pictureBox.Image = global::PictureViewer.Properties.Resources.picture;
             this.pictureBox.Location = new System.Drawing.Point(3, 53);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1140, 607);
+            this.pictureBox.Size = new System.Drawing.Size(1078, 607);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
@@ -181,7 +177,7 @@
             this.buttonPrint.Image = global::PictureViewer.Properties.Resources.print_o4k_icon;
             this.buttonPrint.Location = new System.Drawing.Point(3, 3);
             this.buttonPrint.Name = "buttonPrint";
-            this.buttonPrint.Size = new System.Drawing.Size(280, 44);
+            this.buttonPrint.Size = new System.Drawing.Size(265, 44);
             this.buttonPrint.TabIndex = 1;
             this.buttonPrint.Text = "Print";
             this.buttonPrint.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -194,9 +190,9 @@
             this.buttonCropNormal.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonCropNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonCropNormal.Image = global::PictureViewer.Properties.Resources.crop_VBF_icon;
-            this.buttonCropNormal.Location = new System.Drawing.Point(289, 3);
+            this.buttonCropNormal.Location = new System.Drawing.Point(274, 3);
             this.buttonCropNormal.Name = "buttonCropNormal";
-            this.buttonCropNormal.Size = new System.Drawing.Size(280, 44);
+            this.buttonCropNormal.Size = new System.Drawing.Size(265, 44);
             this.buttonCropNormal.TabIndex = 2;
             this.buttonCropNormal.Text = "Cắt ảnh thường";
             this.buttonCropNormal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -209,9 +205,9 @@
             this.buttonCropFace.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonCropFace.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonCropFace.Image = global::PictureViewer.Properties.Resources.crop_VBF_icon;
-            this.buttonCropFace.Location = new System.Drawing.Point(575, 3);
+            this.buttonCropFace.Location = new System.Drawing.Point(545, 3);
             this.buttonCropFace.Name = "buttonCropFace";
-            this.buttonCropFace.Size = new System.Drawing.Size(280, 44);
+            this.buttonCropFace.Size = new System.Drawing.Size(265, 44);
             this.buttonCropFace.TabIndex = 3;
             this.buttonCropFace.Text = "Cắt ảnh mặt";
             this.buttonCropFace.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -224,9 +220,9 @@
             this.buttonUndo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonUndo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonUndo.Image = global::PictureViewer.Properties.Resources.undo_arrow_gGY_icon;
-            this.buttonUndo.Location = new System.Drawing.Point(861, 3);
+            this.buttonUndo.Location = new System.Drawing.Point(816, 3);
             this.buttonUndo.Name = "buttonUndo";
-            this.buttonUndo.Size = new System.Drawing.Size(282, 44);
+            this.buttonUndo.Size = new System.Drawing.Size(265, 44);
             this.buttonUndo.TabIndex = 4;
             this.buttonUndo.Text = "Quay lại";
             this.buttonUndo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -234,11 +230,15 @@
             this.buttonUndo.UseVisualStyleBackColor = true;
             this.buttonUndo.Click += new System.EventHandler(this.buttonUndo_Click);
             // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1146, 737);
+            this.ClientSize = new System.Drawing.Size(1084, 737);
             this.Controls.Add(this.tableLayout);
             this.Controls.Add(this.menu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
