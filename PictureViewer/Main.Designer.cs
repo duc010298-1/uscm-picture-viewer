44f538ed7dd8d9b1260418a35b794c2740ddcd6d
Do Trung Duc <duc010298@gmail.com>
2021-10-09 22:09:26 +0700

    update

--- PictureViewer/Main.Designer.cs
+++ PictureViewer/Main.Designer.cs
@@ -32,7 +32,7 @@
             this.menu = new System.Windows.Forms.MenuStrip();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openItemMenu = new System.Windows.Forms.ToolStripMenuItem();
-            this.prinyItemMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.printItemMenu = new System.Windows.Forms.ToolStripMenuItem();
             this.setPhotoshopLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitItemMenu = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,6 +45,10 @@
             this.buttonCropFace = new System.Windows.Forms.Button();
             this.buttonUndo = new System.Windows.Forms.Button();
             this.buttonOpenPhotoshop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.buttonAddNote = new System.Windows.Forms.Button();
+            this.buttonRemoveNote = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.menu.SuspendLayout();
             this.tableLayout.SuspendLayout();
@@ -65,7 +69,7 @@
             // 
             this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openItemMenu,
-            this.prinyItemMenu,
+            this.printItemMenu,
             this.setPhotoshopLocationToolStripMenuItem,
             this.toolStripSeparator1,
             this.exitItemMenu});
@@ -80,12 +84,12 @@
             this.openItemMenu.Text = "Mở";
             this.openItemMenu.Click += new System.EventHandler(this.OpenItemMenu_Click);
             // 
-            // prinyItemMenu
+            // printItemMenu
             // 
-            this.prinyItemMenu.Name = "prinyItemMenu";
-            this.prinyItemMenu.Size = new System.Drawing.Size(200, 22);
-            this.prinyItemMenu.Text = "In";
-            this.prinyItemMenu.Click += new System.EventHandler(this.PrinyItemMenu_Click);
+            this.printItemMenu.Name = "printItemMenu";
+            this.printItemMenu.Size = new System.Drawing.Size(200, 22);
+            this.printItemMenu.Text = "In";
+            this.printItemMenu.Click += new System.EventHandler(this.PrintItemMenu_Click);
             // 
             // setPhotoshopLocationToolStripMenuItem
             // 
@@ -109,18 +113,18 @@
             // tableLayout
             // 
             this.tableLayout.ColumnCount = 12;
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
-            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333332F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.333335F));
             this.tableLayout.Controls.Add(this.buttonSelectArea, 0, 2);
             this.tableLayout.Controls.Add(this.buttonCropManual, 1, 2);
             this.tableLayout.Controls.Add(this.pictureBox, 0, 1);
@@ -129,14 +133,19 @@
             this.tableLayout.Controls.Add(this.buttonCropFace, 2, 0);
             this.tableLayout.Controls.Add(this.buttonUndo, 3, 0);
             this.tableLayout.Controls.Add(this.buttonOpenPhotoshop, 2, 2);
+            this.tableLayout.Controls.Add(this.label1, 0, 3);
+            this.tableLayout.Controls.Add(this.textBoxNote, 1, 3);
+            this.tableLayout.Controls.Add(this.buttonAddNote, 8, 3);
+            this.tableLayout.Controls.Add(this.buttonRemoveNote, 10, 3);
             this.tableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayout.Location = new System.Drawing.Point(0, 24);
             this.tableLayout.Name = "tableLayout";
-            this.tableLayout.RowCount = 3;
+            this.tableLayout.RowCount = 4;
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayout.Size = new System.Drawing.Size(1084, 713);
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayout.Size = new System.Drawing.Size(1084, 757);
             this.tableLayout.TabIndex = 0;
             // 
             // buttonSelectArea
@@ -145,7 +154,7 @@
             this.buttonSelectArea.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonSelectArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonSelectArea.Image = global::PictureViewer.Properties.Resources.select_icon;
-            this.buttonSelectArea.Location = new System.Drawing.Point(3, 666);
+            this.buttonSelectArea.Location = new System.Drawing.Point(3, 675);
             this.buttonSelectArea.Name = "buttonSelectArea";
             this.buttonSelectArea.Size = new System.Drawing.Size(354, 44);
             this.buttonSelectArea.TabIndex = 6;
@@ -161,7 +170,7 @@
             this.buttonCropManual.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonCropManual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.buttonCropManual.Image = global::PictureViewer.Properties.Resources.crop_icon;
-            this.buttonCropManual.Location = new System.Drawing.Point(363, 666);
+            this.buttonCropManual.Location = new System.Drawing.Point(363, 675);
             this.buttonCropManual.Name = "buttonCropManual";
             this.buttonCropManual.Size = new System.Drawing.Size(354, 44);
             this.buttonCropManual.TabIndex = 5;
@@ -179,7 +188,7 @@
             this.pictureBox.Image = global::PictureViewer.Properties.Resources.no_image;
             this.pictureBox.Location = new System.Drawing.Point(3, 53);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1078, 607);
+            this.pictureBox.Size = new System.Drawing.Size(1078, 616);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
@@ -258,7 +267,7 @@
             this.buttonOpenPhotoshop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonOpenPhotoshop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonOpenPhotoshop.Image = global::PictureViewer.Properties.Resources.photoshop_icon;
-            this.buttonOpenPhotoshop.Location = new System.Drawing.Point(723, 666);
+            this.buttonOpenPhotoshop.Location = new System.Drawing.Point(723, 675);
             this.buttonOpenPhotoshop.Name = "buttonOpenPhotoshop";
             this.buttonOpenPhotoshop.Size = new System.Drawing.Size(358, 44);
             this.buttonOpenPhotoshop.TabIndex = 7;
@@ -268,11 +277,57 @@
             this.buttonOpenPhotoshop.UseVisualStyleBackColor = true;
             this.buttonOpenPhotoshop.Click += new System.EventHandler(this.ButtonOpenPhotoshop_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 722);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 35);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ghi chú:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxNote
+            // 
+            this.tableLayout.SetColumnSpan(this.textBoxNote, 7);
+            this.textBoxNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNote.Location = new System.Drawing.Point(93, 725);
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.Size = new System.Drawing.Size(624, 29);
+            this.textBoxNote.TabIndex = 9;
+            // 
+            // buttonAddNote
+            // 
+            this.tableLayout.SetColumnSpan(this.buttonAddNote, 2);
+            this.buttonAddNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAddNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddNote.Location = new System.Drawing.Point(723, 725);
+            this.buttonAddNote.Name = "buttonAddNote";
+            this.buttonAddNote.Size = new System.Drawing.Size(174, 29);
+            this.buttonAddNote.TabIndex = 10;
+            this.buttonAddNote.Text = "Thêm ghi chú";
+            this.buttonAddNote.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemoveNote
+            // 
+            this.tableLayout.SetColumnSpan(this.buttonRemoveNote, 2);
+            this.buttonRemoveNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRemoveNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRemoveNote.Location = new System.Drawing.Point(903, 725);
+            this.buttonRemoveNote.Name = "buttonRemoveNote";
+            this.buttonRemoveNote.Size = new System.Drawing.Size(178, 29);
+            this.buttonRemoveNote.TabIndex = 11;
+            this.buttonRemoveNote.Text = "Xóa ghi chú";
+            this.buttonRemoveNote.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1084, 737);
+            this.ClientSize = new System.Drawing.Size(1084, 781);
             this.Controls.Add(this.tableLayout);
             this.Controls.Add(this.menu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -285,6 +340,7 @@
             this.menu.ResumeLayout(false);
             this.menu.PerformLayout();
             this.tableLayout.ResumeLayout(false);
+            this.tableLayout.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -296,7 +352,7 @@
         private System.Windows.Forms.MenuStrip menu;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openItemMenu;
-        private System.Windows.Forms.ToolStripMenuItem prinyItemMenu;
+        private System.Windows.Forms.ToolStripMenuItem printItemMenu;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitItemMenu;
         private System.Windows.Forms.TableLayoutPanel tableLayout;
@@ -310,6 +366,10 @@
         private System.Windows.Forms.Button buttonCropManual;
         private System.Windows.Forms.Button buttonOpenPhotoshop;
         private System.Windows.Forms.ToolStripMenuItem setPhotoshopLocationToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.Button buttonAddNote;
+        private System.Windows.Forms.Button buttonRemoveNote;
     }
 }
 
